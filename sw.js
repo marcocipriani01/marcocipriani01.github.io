313d12e6fd33373012b056e23506d7bac9957da7
marcocipriani01 <marco.cipriani.01@gmail.com>
2021-01-21 10:42:08 +0100

    Fix forms

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const version = '20210118230246';
+const version = '20210121104150';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/2019/08/15/european-space-camp.html","/2019/06/24/Mineral-Moon.html","/2019/06/24/Iris-Nebula.html","/2019/06/13/M57-Whale-Hockey.html","/2019/03/23/sunflower-galaxy.html","/2019/02/05/rosette-ngc2403.html","/2019/01/02/california-nebula.html","/2018/12/27/horsehead-and-crab.html","/2018/12/12/uhc-filter.html","/2018/12/11/wirtanen.html","/projects/CaprileObservatory.html","/projects/IPARCOS.html","/projects/JustAPendulum.html","/projects/MathsTests.html","/projects/RoverBluetooth.html","/projects/ThunderFocus.html","/about.html","/categories.html","/projects/JustAPendulum/diy.html","/blog/","/","/manifest.json","/offline.html","/projects.html","/assets/search.json","/search.html","/assets/styles.css","/thanks.html","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logos/logo.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/2019/08/15/european-space-camp.html","/2019/06/24/Mineral-Moon.html","/2019/06/24/Iris-Nebula.html","/2019/06/13/M57-Whale-Hockey.html","/2019/03/23/sunflower-galaxy.html","/2019/02/05/rosette-ngc2403.html","/2019/01/02/california-nebula.html","/2018/12/27/horsehead-and-crab.html","/2018/12/12/uhc-filter.html","/2018/12/11/wirtanen.html","/projects/CaprileObservatory.html","/projects/IPARCOS.html","/projects/JustAPendulum.html","/projects/MathsTests.html","/projects/RoverBluetooth.html","/projects/ThunderFocus.html","/about.html","/categories.html","/projects/JustAPendulum/diy.html","/blog/","/","/manifest.json","/offline.html","/projects.html","/assets/search.json","/search.html","/assets/styles.css","/redirects.json","/sitemap.xml","/robots.txt","/feed.xml","/assets/logos/logo.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
